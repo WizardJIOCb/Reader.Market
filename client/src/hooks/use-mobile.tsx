6cdac1979e88feab8812f8989895da8f61d2e387
Rodion <rodion89@list.ru>
2026-01-26 21:24:35 +0500

    fix mobile header

--- client/src/hooks/use-mobile.tsx
+++ client/src/hooks/use-mobile.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-const MOBILE_BREAKPOINT = 1100
+const MOBILE_BREAKPOINT = 1135
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
