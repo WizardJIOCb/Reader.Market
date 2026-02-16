2401af583e62ec367046ea698acc75cf253a0eaa
Rodion <rodion89@list.ru>
2026-02-16 14:19:22 +0500

    fix landing news item description

--- shared/cool-redisign/src/sections/LatestNews.tsx
+++ shared/cool-redisign/src/sections/LatestNews.tsx
@@ -100,7 +100,7 @@ export default function LatestNews() {
                 </h3>
 
                 {/* Excerpt */}
-                <p className="text-sm text-muted-foreground mb-4 line-clamp-3 flex-grow">
+                <p style={{ flexGrow: 0.8 }} className="text-sm text-muted-foreground mb-4 line-clamp-3 flex-grow">
                   {item.excerpt}
                 </p>
 
